ad0c7bd5d4c17385df0722ec0ea6f68d0153abf7
Zrimsek, Mike <mike.zrimsek@rovisys.com>
2018-09-13 14:26:56 -0400

    add message service tests

--- 02_Communication/src/app/services/message.service.spec.ts
+++ 02_Communication/src/app/services/message.service.spec.ts
@@ -1,15 +1,32 @@
-import { TestBed, inject } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 
 import { MessageService } from './message.service';
 
 describe('MessageService', () => {
+  let service: MessageService;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [MessageService]
     });
+
+    service = TestBed.get(MessageService);
   });
 
-  it('should be created', inject([MessageService], (service: MessageService) => {
+  it('Should be created', () => {
     expect(service).toBeTruthy();
-  }));
+  });
+
+  it('Should return the message when getMessage is called', () => {
+    const message = service.getMessage();
+    expect(message).toBe('Default Message');
+  });
+
+  it('Should set the message when setMessage is called', () => {
+    const message = 'Some new message';
+    service.setMessage(message);
+
+    const newMessage = service.getMessage();
+    expect(newMessage).toBe(message);
+  });
 });
