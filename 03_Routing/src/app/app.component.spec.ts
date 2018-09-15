b54d111cee59c51a8134918b5ecbee793aa126a7
Mike Zrimsek <mikezrimsek@gmail.com>
2018-09-15 12:13:19 -0400

    fix existing failing tests

--- 03_Routing/src/app/app.component.spec.ts
+++ 03_Routing/src/app/app.component.spec.ts
@@ -1,6 +1,11 @@
-import { TestBed, async } from '@angular/core/testing';
+import { async, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+
 import { AppComponent } from './app.component';
+import { NavComponent } from './components/nav/nav.component';
+
+import { LockService } from './services/lock.service';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -8,24 +13,16 @@ describe('AppComponent', () => {
         RouterTestingModule
       ],
       declarations: [
-        AppComponent
+        AppComponent,
+        NavComponent
       ],
+      providers: [LockService]
     }).compileComponents();
   }));
+
   it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
-  }));
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
-  }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
   }));
 });
--- 03_Routing/src/app/guards/lock.guard.spec.ts
+++ 03_Routing/src/app/guards/lock.guard.spec.ts
@@ -1,11 +1,22 @@
-import { TestBed, async, inject } from '@angular/core/testing';
+import { async, inject, TestBed } from '@angular/core/testing';
+import { Router } from '@angular/router';
+
+import { LockService } from '../services/lock.service';
 
 import { LockGuard } from './lock.guard';
 
 describe('LockGuard', () => {
+  const router = {
+    navigate: jasmine.createSpy('navigate')
+  };
+
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [LockGuard]
+      providers: [
+        LockGuard,
+        LockService,
+        { provide: Router, useValue: router }
+      ]
     });
   });
 
