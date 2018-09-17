ca443516457746616a55e9ddcaad63230d71442f
Mike Zrimsek <mikezrimsek@gmail.com>
2018-09-16 21:08:30 -0400

    add app component tests
    03 testing complete

--- 03_Routing/src/app/app.component.spec.ts
+++ 03_Routing/src/app/app.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { AppComponent } from './app.component';
@@ -7,6 +7,10 @@ import { NavComponent } from './components/nav/nav.component';
 import { LockService } from './services/lock.service';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  let service: LockService;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
@@ -20,9 +24,23 @@ describe('AppComponent', () => {
     }).compileComponents();
   }));
 
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.debugElement.componentInstance;
+    service = TestBed.get(LockService);
+
+    spyOn(service, 'unlock').and.callThrough();
+
+    fixture.detectChanges();
+  });
+
   it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
+    expect(component).toBeTruthy();
   }));
+
+  it('Should call lockService unlock when unlock button is clicked', () => {
+    const button = fixture.nativeElement.querySelector('#unlock-button');
+    button.click();
+    expect(service.unlock).toHaveBeenCalled();
+  });
 });
