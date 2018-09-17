5bafda467fa3fcd30f56ceafeebe96cc9a2fe155
Mike Zrimsek <mikezrimsek@gmail.com>
2018-09-16 21:04:08 -0400

    add lock service tests

--- 03_Routing/src/app/services/lock.service.spec.ts
+++ 03_Routing/src/app/services/lock.service.spec.ts
@@ -1,15 +1,30 @@
-import { TestBed, inject } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 
 import { LockService } from './lock.service';
 
 describe('LockService', () => {
+  let service: LockService;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [LockService]
     });
+
+    service = TestBed.get(LockService);
   });
 
-  it('should be created', inject([LockService], (service: LockService) => {
+  it('should be created', () => {
     expect(service).toBeTruthy();
-  }));
+  });
+
+  it('Should return true when isLocked is called', () => {
+    const result = service.isLocked();
+    expect(result).toBe(true);
+  });
+
+  it('Should return false when isLocked is called after unlock', () => {
+    service.unlock();
+    const result = service.isLocked();
+    expect(result).toBe(false);
+  });
 });
